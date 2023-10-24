a0b265b316dd97472ac98cbba5ae50662f146135
vincjo <v.joseph3@gmail.com>
2023-10-24 15:45:27 +0200

    fix: empty string to null value

--- client/src/lib/utils/index.ts
+++ client/src/lib/utils/index.ts
@@ -1,8 +1,11 @@
 
 
 export const removeEmptyString = (json: Object) => {
-    return Object.fromEntries(Object.entries(json).filter(([_, v]) => v !== ''))
+    // return Object.fromEntries(Object.entries(json).filter(([_, v]) => v !== ''))
+    Object.keys(json).forEach(k => json[k] = json[k] === '' ? null : json[k])
+    return json
 }
+
 
 export const removeNullEntries = (json: Object) => {
     return Object.fromEntries(Object.entries(json).filter(([_, v]) => v !== null))
