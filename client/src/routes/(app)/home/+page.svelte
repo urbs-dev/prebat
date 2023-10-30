fc14788bb2670dca10080a965a79c5aff8f85295
vincjo <v.joseph3@gmail.com>
2023-10-30 12:22:30 +0100

    update home page

--- client/src/routes/(app)/home/+page.svelte
+++ client/src/routes/(app)/home/+page.svelte
@@ -25,7 +25,7 @@
         <!-- <p>Ces constats sont disponibles aux trois niveaux de traitement suivants&nbsp;:</p> -->
 
         <h1 class="alt-font">PREBAT&nbsp;: Plateforme Data des performances réelles en bâtiment</h1>
-        <p style:margin-top="40px">En tant qu'acteur de la construction, rénovation, gestion ou exploitation de bâtiment résidentiel ou tertiaire, comme en tant qu'occupant, utilisateur ou usager, nous agissons tous sur leur performance effective. Le Cerema en donne ici ses constats techniques et d'usage, en conditions réelles de fonctionnement et d'occupation, par contexte, en type de bâtiment, nature de travaux, équipements, localisation... et aux trois niveaux de traitement suivants&nbsp;:</p>
+        <p style:margin-top="24px">En tant qu'acteur de la construction, rénovation, gestion ou exploitation de bâtiment résidentiel ou tertiaire, comme en tant qu'occupant, utilisateur ou usager, nous agissons tous sur leur performance effective. Le Cerema en donne ici ses constats techniques et d'usage, en conditions réelles de fonctionnement et d'occupation, par contexte, en type de bâtiment, nature de travaux, équipements, localisation... et aux trois niveaux de traitement suivants&nbsp;:</p>
 
         <ul>
             <li>Données élémentaires par bâtiment&nbsp;: mesures, observations et enquêtes;</li>
@@ -38,28 +38,24 @@
         <!-- <p style:margin-top="40px">Données&nbsp;: tous les postes de consommation, performances du bâti et des équipements techniques, pratiques des acteurs et usage, notamment confort thermique. Mesures pendant les deux premières années d'occupation.</p> -->
         <h1 class="alt-font">Énergie et confort thermique</h1>
 
-        <h2 class="flex" style:margin-top="40px">
+        <h2 class="flex" style:margin-top="24px">
             <i class="micon" style:margin-right="8px">navigate_next</i>
             Échantillon
         </h2>
         <p>166 opérations de bâtiments performants (niveau RT2012).</p>
         <img style:width="80%" src="STATIC_PATH/img/energie_confort_thermique.png" alt="Graphique énergie et confort thermique">
 
-        <div class="h3">
-            <h3><span>1.</span> Évaluations par opération</h3>
-            <a href="{getPath('/measures')}">
-                <button class="btn" type="button">
-                    <i class="micon">file_download</i>
-                    <span>Télécharger les données de mesure par opération</span>
-                </button>
-            </a>
-            <h3><span>2.</span> Résultats d'évaluation des opérations</h3>
-            <button class="btn" type="button" on:click={download}>
-                <i class="micon">file_download</i>
-                <span>Télécharger les résultats pour toutes les opérations</span>
-            </button>
-            <h3><span>3.</span> Enseignements inter-opérations</h3>
+        <h2 class="flex" style:margin-top="40px">
+            <i class="micon" style:margin-right="8px">navigate_next</i>
+            Données et enseignements
+        </h2>
 
+        <div class="h3">
+            <h3><span>1.</span>Données et enseignements inter-opérations</h3>
+            <div class="flex">
+                <img style="width:80%;" src="STATIC_PATH/img/enseignement_inter_operations.png" alt="Graphique enseignement inter-opérations">
+                <span>Consommations totales en énergie primaire par m² SHON et par an</span>
+            </div>
             <a href="https://www.cerema.fr/fr/centre-ressources/boutique/batiments-demonstrateurs-basse-consommation-energie-prebat-3">
                 <button class="btn" type="button">
                     <i class="micon">exit_to_app</i>
@@ -79,7 +75,18 @@
                 </button>
             </a>
 
-            <img style="width:80%;padding-left: 40px;" src="STATIC_PATH/img/enseignement_inter_operations.png" alt="Graphique enseignement inter-opérations">
+            <button class="btn" type="button" on:click={download}>
+                <i class="micon">file_download</i>
+                <span>RESULTATS D'EVALUATION : télécharger les données pour toutes les opérations</span>
+            </button>
+
+            <h3 style:margin-top="32px"><span>2.</span>Données par opération</h3>
+            <a href="{getPath('/measures')}">
+                <button class="btn" type="button">
+                    <i class="micon">file_download</i>
+                    <span>MESURES : sélectionner et télécharger les données par opération</span>
+                </button>
+            </a>
 
         </div>
 
@@ -104,11 +111,11 @@
     }
     section {
         max-width: 960px;
-        margin: 40px auto;
+        margin: 24px auto;
         padding-bottom: 96px;
         border: 1px solid #eee;
         border-radius: 8px;
-        padding: 24px 40px;
+        padding: 0 40px 40px 40px;
     }
 
     section 
@@ -119,19 +126,19 @@
     h1 {
         display: inline;
         color: var(--primary-darken);
-        /* background: linear-gradient(180deg,rgba(255,255,255,0) 75%, var(--primary-lighten-1) 15%); */
-        font-size: 28px;
+        background: linear-gradient(180deg,rgba(255,255,255,0) 75%, var(--primary-lighten-1) 15%);
+        font-size: 24px;
     }
     h2 {
         color: var(--primary-lighten);
-        font-size: 24px;
+        font-size: 20px;
         font-family: Lato;
         margin-top: 32px;
     }
     h3 {
         color: var(--primary);
-        font-size: 20px;
-        margin-top: 32px;
+        font-size: 18px;
+        margin-top: 16px;
         margin-bottom: 8px;
     }
     h3 span {
@@ -139,10 +146,10 @@
         padding-right: 16px;
     }
     p, ul {
-        font-size: 17px;
+        font-size: 15px;
         font-weight: lighter;
         color: #616161;
-        line-height: 24px;
+        line-height: 20px;
     }
     ul {
         list-style-type: decimal;
@@ -186,5 +193,20 @@
         color: var(--secondary);
         font-size: 20px;
         margin-right: 8px;
+    }
+
+    div.h3 div.flex {
+        flex-direction: column;
+        align-items: flex-start;
+        padding-left: 40px;
+        margin-bottom: 40px;
+    }
+    div.h3 div.flex span {
+        border-top: 1px solid #e0e0e0;
+        color: #616161;
+        font-family: JetBrains;
+        font-size: 13px;
+        width: 80%;
+        padding-top: 4px;
     }
 </style>
