83b5433dfe39488fa6ec617e34e98c8ef0328e09
Ethan <ethanvillesseche@urbs.fr>
2024-03-19 12:08:27 +0100

    hide first part of footer

--- client/src/lib/components/Footer.svelte
+++ client/src/lib/components/Footer.svelte
@@ -6,8 +6,8 @@
 
 
 <footer>
-    <section>
-        <div>
+     <section style="display: none;">
+        <!--<div>
             <span>
                 Menu pied de page 1 
             </span>
@@ -62,8 +62,8 @@
                     </a>
                 </li>
             </ul>
-        </div>
-    </section>
+        </div>-->
+    </section> 
     <section>
         <aside class="flex">
             <img style:height="140px" src="STATIC_PATH/img/logo-rf.svg" alt="logo République Française">
@@ -91,6 +91,18 @@
                         expertises-territoires.fr
                     </a>
                 </li>
+                <ul>
+                    <li>
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23"/></svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" fill-rule="evenodd" d="M9.429 8.969h3.714v1.85c.535-1.064 1.907-2.02 3.968-2.02c3.951 0 4.889 2.118 4.889 6.004V22h-4v-6.312c0-2.213-.535-3.461-1.897-3.461c-1.889 0-2.674 1.345-2.674 3.46V22h-4zM2.57 21.83h4V8.799h-4zM7.143 4.55a2.53 2.53 0 0 1-.753 1.802a2.573 2.573 0 0 1-1.82.748a2.59 2.59 0 0 1-1.818-.747A2.548 2.548 0 0 1 2 4.55c0-.677.27-1.325.753-1.803A2.583 2.583 0 0 1 4.571 2c.682 0 1.336.269 1.819.747c.482.478.753 1.126.753 1.803" clip-rule="evenodd"/></svg>
+                        </a>
+                    </li>
+                </ul>
             <!-- <span>Copyright 2023 © - Cerema</span> -->
             <!-- <a href="https://www.cerema.fr/fr/contact/form?source_entity_type=taxonomy_term&source_entity_id=1797&category=1797&message=Bonjour,%0AJe%20vous%20contacte%20suite%20%C3%A0%20ma%20visite%20sur%20le%20site%20avatar.cerema.fr.%0AJe%20souhaiterais%20prendre%20contact%20avec%20une%20personne%20de%20l%27%C3%A9quipe%20projet%20du%20Cerema.%20Merci%20de%20leur%20transmettre%20mes%20coordonn%C3%A9es.%0AVous%20remerciant%20par%20avance,%0ACordialement" rel="external">Nous contacter</a> -->
             <!-- <a href="{getPath('/legal')}">Mentions légales</a> -->
@@ -384,8 +396,8 @@
     footer {
         position: relative;
         height: 200px;
+        border-top: 5px solid var(--ternary);
         background: #fafafa;
-        border-top: 1px solid var(--primary-lighten-1)
     }
     section {
         display: flex;
@@ -394,7 +406,6 @@
         padding: 2em 240px 2em 240px ;
     }
     section:nth-child(1) {
-        border-top: 4px solid var(--ternary);
         background-color: var(--primary-lighten-1);
         padding: 5em;
     }
