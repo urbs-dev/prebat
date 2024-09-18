ca62f88f93549bec0390429e65a171ba0ccef2cb
Ethan <ethanvillesseche@urbs.fr>
2024-09-18 17:25:10 +0200

    ✨ feat: add POST,PUT,DELETE route to scenarios

---
+++ api/src/Scenarios/ScenariosController.ts
@@ -0,0 +1,48 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import ScenariosModel from './ScenariosModel'
+import OperationsModel from 'App/Operations/OperationsModel'
+
+export default class ScenariosController 
+{
+   
+    public async store({request, response }: HttpContextContract)
+    {
+        const scenario = request.all()
+        if( scenario?.operation_id === undefined ) 
+            return response.status(400).send({ message: 'operation_id is required' })
+
+        if ( !await OperationsModel.find(scenario.operation_id) )
+            return response.status(404).send({ message: 'Operation not found' })
+
+        console.log(scenario);
+        const result = await ScenariosModel.create(scenario)
+        return response.send(result)
+    }
+
+    public async update({request, response }: HttpContextContract)
+    {   
+        const id = request.param('id')
+        const scenario = request.all()
+
+        const result = await ScenariosModel.find(id)
+        
+        if( !result )
+            return response.status(404).send({ message: 'Scenario not found' })
+
+        await result.merge(scenario).save()
+        return response.send(scenario)
+    }
+
+    public async destroy({request, response }: HttpContextContract)
+    {
+        const id = request.param('id')
+        const scenario = await ScenariosModel.find(id)
+
+        if( !scenario )
+            return response.status(404).send({ message: 'Scenario not found' })
+
+        await scenario.delete()
+        return response.send({ message: 'Scenario deleted' })
+    }
+}
